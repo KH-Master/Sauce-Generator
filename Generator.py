e54d40e79c63412264b9aa97ec8c1def4fefb92f
KH-Master <92768247+KH-Master@users.noreply.github.com>
2021-10-18 21:10:48 -0500

    Add files via upload

---
+++ Generator.py
@@ -0,0 +1,26 @@
+import winsound
+import random 
+winsound.PlaySound("show.wav", winsound.SND_ASYNC)
+sp = "splat.wav"
+d = "drum.wav"
+f = "fun.wav"
+print("WELCOME TO THE RANDOM SAUCE GENERATOR 9000! " + "\n")
+n = input("Press ENTER to continue. ")
+print("Here's the deal (you horny bastard).")
+n2 = input("Press ENTER to continue. ")
+print("This here thingy-matron is gonna generate a totally random")
+print("(and potentially disgusting) ")
+print("nhentai link!" + "\n")
+n3 = input("Press ENTER to continue. ")
+print("After you press ENTER, you'll get a custom nhentai link that you can enjoy to your heart's content." + "\n")
+print("So get ready chump because here we go! " + "\n")
+n4 = input("Press ENTER to get your link. ")
+winsound.PlaySound(d, winsound.SND_FILENAME)
+print("Your custom nhentai link is..." + "\n")
+nhen = random.randint(100000,400000)
+print("https://nhentai.net/g/" + str(nhen))
+winsound.PlaySound(sp, winsound.SND_FILENAME)
+winsound.PlaySound(f, winsound.SND_ASYNC)
+print("Donate if you want. It will give me more motivation to do bigger and better projects. I'll learn more as I go lmao." + "\n")
+print("My Cash App is: $khmastr")
+n2 = input("Press anything to quit...you filthy animal. ")
